bf97dce92d060daefc4e34fc6a1bd12dcb1fe60d
Tomas Knapen <tknapen@gmail.com>
2019-08-09 15:21:18 +0200

    neurodocker updated

--- base/neurodocker.sh
+++ base/neurodocker.sh
@@ -1,4 +1,4 @@
-version=0.0.10nvtest
+version=0.0.11
 
 #####################################################################
 ## create docker image, with installations
@@ -8,19 +8,22 @@ docker run --rm kaczmarj/neurodocker:master \
 generate docker --base debian:stretch --pkg-manager apt \
 --user=root \
 --run 'mkdir /data && chmod 777 /data && chmod a+s /data' \
---install git gcc g++ gfortran inkscape \
+--install git gcc g++ gfortran inkscape nano \
 --afni version=latest method=binaries \
---ants version=2.2.0 \
---freesurfer version=6.0.1 license_path=../licenses/freesurfer_license.txt \
---fsl version=5.0.11 \
+--ants version=2.2.0 method=binaries \
+--freesurfer version=6.0.1 license_path=license.txt \
+--fsl version=5.0.11  method=binaries \
+--convert3d version=1.0.0 method=binaries \
 --dcm2niix version=latest method=source \
+--matlabmcr version=2012b method=binaries \
 --copy py36_nov.yml /tmp/environment.yml \
 --miniconda create_env=neuro yaml_file="/tmp/environment.yml" activate=true \
 --copy jupyter_notebook_config.py /etc/jupyter/jupyter_notebook_config.py \
 --add-to-entrypoint "source activate neuro" \
---add-to-entrypoint "jupyter lab --ip 0.0.0.0 --no-browser --config=/etc/jupyter/jupyter_notebook_config.py" \
+--neurodebian os_codename=stretch server=usa-nh --install connectome-workbench \
 --workdir /data > Dockerfile
 
+# --add-to-entrypoint "jupyter lab --ip 0.0.0.0 --no-browser --config=/etc/jupyter/jupyter_notebook_config.py" \
 # --user=neuro \
 
 # options not used
@@ -49,14 +52,14 @@ docker push knapenlab/nd:${version}
 ### to convert to singularity
 #####################################################################
 
-docker run \
--v /var/run/docker.sock:/var/run/docker.sock \
--v /home/shared/software/knapenlab/kl-${version}:/output \
---privileged -t --rm \
-singularityware/docker2singularity \
-knapenlab/nd:${version}
+# docker run \
+# -v /var/run/docker.sock:/var/run/docker.sock \
+# -v /home/shared/software/knapenlab/kl-${version}:/output \
+# --privileged -t --rm \
+# singularityware/docker2singularity \
+# knapenlab/nd:${version}
 
-
+singularity build knapenlab-$VERSION.simg docker://knapenlab/nd:$VERSION
 
 #####################################################################
 ## set up docker image, select which volumes to mount from host
@@ -89,7 +92,7 @@ docker run -p ${external_portnr}:${internal_portnr} --expose=${external_portnr} 
 ### as singularity image
 ### not working yet, needs to do port mapping which is problematic
 ### PYTHONPATH="" singularity run --bind /home/shared \
-### --bind /home/raw_data/ /home/shared/software/knapenlab/kl-0.0.2test 
+### --bind /home/raw_data/ /home/shared/software/knapenlab/kl-0.0.2test
 #####################################################################
 
 
